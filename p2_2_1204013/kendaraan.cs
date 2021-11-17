461cd97dd8831c34cc3148eb8f77bf9f6ede3d7b
fauziahenni <87329906+fauziahenni@users.noreply.github.com>
2021-11-17 10:08:19 -0800

    Add files via upload

---
+++ p2_2_1204013/kendaraan.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2_2_1204013
+{
+    public class kendaraan
+    {
+        private string nama;
+        private string type;
+        public kendaraan(string nama, string type) {
+            this.nama = nama;
+            this.type = type;
+        }
+        public string Nama
+        {
+            get
+            {
+                return nama;
+            }
+            set
+            {
+                nama = value;
+
+            }
+
+        }
+        public string Type
+        {
+            get
+            {
+                return type;
+
+            }
+            set
+            {
+                type = value;
+            }
+        }
+    }
+ }
+
---
+++ p2_2_1204013/mobil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2_2_1204013
+{
+   public class mobil:kendaraan
+    {
+        protected string warna;
+        public mobil (string type,string warna): base( type,warna)
+        {
+            this.Nama = "BMW";
+            this.Warna = warna;
+            this.Type = type;
+        }
+        public string Warna
+        {
+            get
+            {
+                return warna;
+            }
+            set
+            {
+                warna = value;
+            }
+        }
+       
+    }
+}
